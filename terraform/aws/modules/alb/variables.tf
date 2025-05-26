1843405a65198cfc511db598325023bf23420306
rahulBioFuel <jsd1@biofuelsjunction.com>
2025-05-26 17:47:06 +0530

    iac: terraform aws changes

---
+++ terraform/aws/modules/alb/variables.tf
@@ -0,0 +1,24 @@
+variable "alb_name" {
+  description = "Name of the ALB"
+  type        = string
+}
+
+variable "sg_id" {
+  description = "Security Group ID for the ALB"
+  type        = string
+}
+
+variable "subnets" {
+  description = "Subnets for the ALB"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the ALB resides"
+  type        = string
+}
+
+# variable "instance_id_jenkins" {
+#   description = "VPC ID where the ALB resides"
+#   type        = string
+# }
